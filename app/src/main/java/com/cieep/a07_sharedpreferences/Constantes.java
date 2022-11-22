65b4ff0d213f589b5a3b721b1185ddd5531eba8e
eduard <eduard@cieep.com>
2022-11-22 13:38:52 +0100

    Initial commit

---
+++ app/src/main/java/com/cieep/a07_sharedpreferences/Constantes.java
@@ -0,0 +1,12 @@
+package com.cieep.a07_sharedpreferences;
+
+public class Constantes {
+
+    // Constantes para las SharedPreferences
+    public static final String PERSONAS = "personas";
+
+    // Constantes para las keys de los valores
+    public static final String NOMBRE = "nombre";
+    public static final String EDAD = "edad";
+
+}
